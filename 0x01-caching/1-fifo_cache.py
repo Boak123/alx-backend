db46af93f2280c32b5a3c857dcfd1e10879a5377
boak123 <boluwatifeakorede17@gmail.com>
2024-04-03 23:53:48 +0000

    caching policies

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
-""" class FIFOCache that inherits from BaseCaching and is a caching system
-"""
 
+'''Task 1: FIFO caching
+'''
+
+
+from collections import OrderedDict
 from base_caching import BaseCaching
 
+
 class FIFOCache(BaseCaching):
-    """ A FIFO class that inherits from
-    BaseCaching and is a caching system
-    """
-    def def __init__(self):
+    '''A class `FIFOCache` that inherits from
+       `BaseCaching` and is a caching system.
+    '''
+
+    def __init__(self):
         super().__init__()
-        
+        self.cache_data = OrderedDict()
+
     def put(self, key, item):
-        """ assign to the dictionary `self.cache_data` the
-           `item` value for the key `key
-        """
-        
-        if key is None and item is None:
+        '''assign to the dictionary `self.cache_data` the
+           `item` value for the key `key`
+        '''
+
+        if key is None or item is None:
             return
-        
+
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             first_key, _ = self.cache_data.popitem(last=False)
             print(f"DISCARD: {first_key}")
-            
+
         self.cache_data[key] = item
-        
+
     def get(self, key):
-        """ return the value in `self.cache_data` linked to `key`
-        """
-       return self.cache_data.get(key, None)
+        '''return the value in `self.cache_data` linked to `key`
+        '''
+        return self.cache_data.get(key, None)
